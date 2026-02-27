0dfe7eef054bfce95e9516ecd8267d69a07de853
Ozair-Khan1 <ozairk.work@gmail.com>
2026-02-27 15:15:35 +0500

    jolly btns check 2

--- public/assets/Jolly/Jolly-Roger.service.worker.js
+++ public/assets/Jolly/Jolly-Roger.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1772186767|77921899';
+const CACHE_VERSION = '1772187258|69083376';
 /** @type {string} */
 const CACHE_PREFIX = 'Jolly Roger-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
