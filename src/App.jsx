db63d9800cf97a84cd66c2b36cc066fb1f10149b
Ozair-Khan1 <ozairk.work@gmail.com>
2026-02-27 15:43:38 +0500

    games final touches 2

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { easeInOut, motion } from 'framer-motion'
 import { Navbar } from './Component/Navbar'
 import { Home } from './Component/Home'
 import { Outlet } from 'react-router-dom'
+import { Link } from 'react-router-dom'
 
 function App() {
     const randomX = [0, 50, 20, 30, 40, 0]
@@ -18,6 +19,9 @@ function App() {
         <div className='bento-body'>
          <Outlet/>
         </div>
+        <footer className='d-flex justify-content-center align-items-center mt-2'>
+          <p className='' style={{color : 'rgba(240, 242, 245, 0.7)'}}>@ O.K Folio 2024, Design By <Link to='/' className='text-decoration-none'><span>O.K Folio</span></Link></p>
+        </footer>
     </>
   )
 }
