027505c48cf3b7621419b71cc3c5ed3eb9c8baa7
Ozair-Khan1 <ozairk.work@gmail.com>
2026-03-03 18:56:10 +0500

    cv fixed 4

--- src/Component/Navbar.jsx
+++ src/Component/Navbar.jsx
@@ -127,11 +127,9 @@ export const Navbar = () => {
                                 </li>  
                                 </ul>
                                 <div className="d-block mt-5">     
-                                <NavLink to='/Contact'>
                                     <motion.a href="/assets/Ozair-Khan-CV.pdf" download="Ozair-Khan-CV.pdf" className="btn text-white border-0 py-3 px-4 d-inline-flex gap-2" whileHover={{backgroundColor : "#0D6EFD"}} transition={{duration : 0, ease : easeInOut}} style={{backgroundColor : "#0E1018"}}>
                                     <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-download-icon lucide-download"><path d="M12 15V3"/><path d="M21 15v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4"/><path d="m7 10 5 5 5-5"/></svg> Download CV
                                     </motion.a>
-                                </NavLink>
                                 </div>
                             </div>
                         </motion.div>
